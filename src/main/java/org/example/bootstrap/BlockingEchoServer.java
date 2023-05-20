80434214e2f9723e5715a62ca29e2aeaa0486939
jiemu <rebianne03@naver.com>
2023-05-21 01:41:54 +0900

    2023.05.18 netty-project - bootstrap example

--- src/main/java/org/example/bootstrap/BlockingEchoServer.java
+++ src/main/java/org/example/bootstrap/BlockingEchoServer.java
@@ -1,25 +1,26 @@
-package org.example.echo;
+package org.example.bootstrap;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.EventLoopGroup;
-import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.oio.OioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
-import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.oio.OioServerSocketChannel;
+import org.example.echo.EchoServerHandler;
 
-import java.awt.*;
-
-public class EchoServer {
+// 블로킹 모드로 하고 싶다면?
+// Nio -> Oio
+public class BlockingEchoServer {
     public static void main(String[] args) throws Exception {
-        EventLoopGroup bossGroup = new NioEventLoopGroup(1); // 스레드 수 1
-        EventLoopGroup workerGroup = new NioEventLoopGroup(); // CPU 코어 수에 따른 스레드 수 설정
+        EventLoopGroup bossGroup = new OioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new OioEventLoopGroup();
 
         try {
             ServerBootstrap b = new ServerBootstrap();
-            b.group(bossGroup, workerGroup) // 첫번째: 부모 스레드, 두번째: 연결된 소켓에 대한 I/O 처리를 담당하는 자식 스레드
-                    .channel(NioServerSocketChannel.class)
+            b.group(bossGroup, workerGroup)
+                    .channel(OioServerSocketChannel.class)
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         public void initChannel(SocketChannel ch) {
@@ -35,6 +36,5 @@ public class EchoServer {
             workerGroup.shutdownGracefully();
             bossGroup.shutdownGracefully();
         }
-
     }
 }
--- src/main/java/org/example/bootstrap/EpollEchoServer.java
+++ src/main/java/org/example/bootstrap/EpollEchoServer.java
@@ -1,25 +1,25 @@
-package org.example.echo;
+package org.example.bootstrap;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.EventLoopGroup;
-import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.epoll.EpollEventLoopGroup;
+import io.netty.channel.epoll.EpollServerSocketChannel;
 import io.netty.channel.socket.SocketChannel;
-import io.netty.channel.socket.nio.NioServerSocketChannel;
+import org.example.echo.EchoServerHandler;
 
-import java.awt.*;
-
-public class EchoServer {
+// Epoll 입출력 모드를 지원하는 서버 애플리케이션
+public class EpollEchoServer {
     public static void main(String[] args) throws Exception {
-        EventLoopGroup bossGroup = new NioEventLoopGroup(1); // 스레드 수 1
-        EventLoopGroup workerGroup = new NioEventLoopGroup(); // CPU 코어 수에 따른 스레드 수 설정
+        EventLoopGroup bossGroup = new EpollEventLoopGroup(1);
+        EventLoopGroup workerGroup = new EpollEventLoopGroup();
 
         try {
             ServerBootstrap b = new ServerBootstrap();
-            b.group(bossGroup, workerGroup) // 첫번째: 부모 스레드, 두번째: 연결된 소켓에 대한 I/O 처리를 담당하는 자식 스레드
-                    .channel(NioServerSocketChannel.class)
+            b.group(bossGroup, workerGroup)
+                    .channel(EpollServerSocketChannel.class)
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         public void initChannel(SocketChannel ch) {
@@ -35,6 +35,5 @@ public class EchoServer {
             workerGroup.shutdownGracefully();
             bossGroup.shutdownGracefully();
         }
-
     }
 }
